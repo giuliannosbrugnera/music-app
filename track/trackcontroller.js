2b08eac91637f20c4aa994f02ee9739a530b6ca2
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-31 14:18:27 -0300

    Fixed some issues

--- track/trackcontroller.js
+++ track/trackcontroller.js
@@ -9,31 +9,14 @@
  * Controller for the Track of the musicApp.
  */
 function TrackCtrl($scope, $stateParams, TrackService) {
-    $scope.tracks = []; // Declare an empty array.
-    $scope.pageSize = $stateParams.pageSize === null ? 10 : $stateParams.pageSize;
-    $scope.pageNumber = $stateParams.pageNumber === null ? 1 : $stateParams.pageNumber; // Initialize page number to 1 if not set.
-    $scope.orderBy = $stateParams.orderBy === null ? "Name" : $stateParams.orderBy;
-    $scope.totalCount = 0;
-    $scope.totalPages = 0;
-
-    if ($stateParams.action === "view") {
-        // Should get a specific track.
-        $scope.track = getOneTrack($stateParams.id);
-    } else {
-        // Should get all the tracks.
-        getAllTracks($scope.pageSize, $scope.pageNumber, $scope.orderBy);
-    }
-
-    function getOneTrack(id) {
-        var result = TrackService.get({ id: id });
-        result.$promise.then(function(data) {
-            console.log(JSON.stringify(data));
+    $scope.getOneTrack = function(id) {
+        $scope.track = TrackService.get({ id: id });
+        $scope.track.$promise.then(function(data) {
+            $scope.track = data;
         });
-
-        return result;
     }
 
-    function getAllTracks(pageSize, pageNumber, orderBy) {
+    $scope.getAllTracks = function(pageSize, pageNumber, orderBy) {
         // Does not return the actual data immediately.
         // It returns something will hold the data when the ajax returns.
         // Fetch all tracks. Issues a GET to /api/tracks/:pageSize/:pageNumber/:orderBy
@@ -46,8 +29,24 @@ function TrackCtrl($scope, $stateParams, TrackService) {
     }
 
     $scope.pageChanged = function(newPage) {
-        getAllTracks($scope.pageSize, newPage, $scope.orderBy);
+        $scope.getAllTracks($scope.pageSize, newPage, $scope.orderBy);
     };
+
+    $scope.track = {};
+    $scope.tracks = []; // Declare an empty array.
+    $scope.pageSize = $stateParams.pageSize === null ? 10 : $stateParams.pageSize;
+    $scope.pageNumber = $stateParams.pageNumber === null ? 1 : $stateParams.pageNumber; // Initialize page number to 1 if not set.
+    $scope.orderBy = $stateParams.orderBy === null ? "Name" : $stateParams.orderBy;
+    $scope.totalCount = 0;
+    $scope.totalPages = 0;
+
+    if ($stateParams.action === "view") {
+        // Should get a specific track.
+        $scope.getOneTrack($stateParams.id);
+    } else {
+        // Should get all the tracks.
+        $scope.getAllTracks($scope.pageSize, $scope.pageNumber, $scope.orderBy);
+    }
 }
 
 angular.module('musicApp').controller('TrackCtrl', ['$scope', '$stateParams', 'TrackService', TrackCtrl]);
